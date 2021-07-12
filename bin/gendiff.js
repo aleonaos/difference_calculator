b669fd00e6a7d73bc1d43218030066be13b198b0
unknown <aleonaosotova@yandex.ru>
2021-07-12 18:18:38 +0300

    step 2: add format, version

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,6 +2,9 @@
 import program from 'commander';
 
 program
+  .version('0.0.1')
+  .argument('<filepath1> <filepath2>')
   .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'output format')
 
 program.parse(process.argv);
