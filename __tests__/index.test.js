1d7ed0ca9adc89b0c3d4698c0eefaaa06cb5fc28
unknown <aleonaosotova@yandex.ru>
2021-07-14 14:26:26 +0300

    add test flat files

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,7 +1,6 @@
 import { test, expect } from '@jest/globals';
 import { fileURLToPath } from 'url';
 import path from 'path';
-import fs from 'fs';
 import genDiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
