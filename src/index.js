3f2aa17c126950ee39c1be51cd1bc3b3e4b72e87
unknown <aleonaosotova@yandex.ru>
2021-07-14 15:05:29 +0300

    change index.js

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ import _ from 'lodash';
 
 const readJSON = (file) => {
   const jsonFile = fs.readFileSync(path.resolve(file), 'utf-8');
+  const parseFile = JSON.parse(jsonFile);
 
-  return JSON.parse(jsonFile);
+  return parseFile;
 };
 
 const operators = ['-', '+'];
