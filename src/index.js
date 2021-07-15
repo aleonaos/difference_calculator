904960701436db254337ae4346ed10bc6d3d4c49
unknown <aleonaosotova@yandex.ru>
2021-07-15 11:59:32 +0300

    change index.js for work with json and yml files

--- src/index.js
+++ src/index.js
@@ -1,27 +1,31 @@
 import path from 'path';
 import fs from 'fs';
 import _ from 'lodash';
+import parse from './parsers.js';
 
-const readJSON = (file) => {
-  const jsonFile = fs.readFileSync(path.resolve(file), 'utf-8');
-  const parseFile = JSON.parse(jsonFile);
+const makeFileData = (filepath) => {
+  const data = fs.readFileSync(path.resolve(filepath), 'utf-8');
+  const type = path.extname(filepath).slice(1);
 
-  return parseFile;
+  return { data, type };
 };
 
 const operators = ['-', '+'];
 
 const genDiff = (filepath1, filepath2) => {
-  const parse1 = readJSON(filepath1);
-  const parse2 = readJSON(filepath2);
+  const fileData1 = makeFileData(filepath1);
+  const fileData2 = makeFileData(filepath2);
 
-  const keys1 = Object.keys(parse1);
-  const keys2 = Object.keys(parse2);
+  const parsedFile1 = parse(fileData1.type, fileData1.data);
+  const parsedFile2 = parse(fileData2.type, fileData2.data);
+
+  const keys1 = Object.keys(parsedFile1);
+  const keys2 = Object.keys(parsedFile2);
   const combineKeys = _.union(keys1, keys2).sort();
 
   const arrayOfKeys = combineKeys.reduce((acc, key) => {
-    const value1 = parse1[key];
-    const value2 = parse2[key];
+    const value1 = parsedFile1[key];
+    const value2 = parsedFile2[key];
     const indent = '  ';
 
     if (!keys2.includes(key)) {
@@ -31,7 +35,7 @@ const genDiff = (filepath1, filepath2) => {
       return [...acc, `${indent}${operators[1]} ${key}: ${value2}`];
     }
     if (value1 === value2) {
-      return [...acc, `${indent.repeat(2)}${key}: ${value1}`];
+      return [...acc, `${indent.repeat(2)}${key}: ${value2}`];
     }
 
     return [...acc, `${indent}${operators[0]} ${key}: ${value1}`, `${indent}${operators[1]} ${key}: ${value2}`];
