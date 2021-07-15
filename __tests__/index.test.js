b5163efd4a88ec172b1d3bd5bb6a63a851925ba8
unknown <aleonaosotova@yandex.ru>
2021-07-15 11:26:34 +0300

    add yml test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,15 +1,24 @@
-import { test, expect } from '@jest/globals';
+import { test, expect, describe } from '@jest/globals';
 import { fileURLToPath } from 'url';
 import path from 'path';
+import fs from 'fs';
 import genDiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', `/${filename}`);
 
-test('flat', () => {
-  const expected = '{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}';
-  const before = getFixturePath('file1.json');
-  const after = getFixturePath('file2.json');
-  expect(genDiff(before, after)).toBe(expected);
+describe('gendiff', () => {
+  test('flat json files', () => {
+    const expected = '{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}';
+    const before = getFixturePath('file1.json');
+    const after = getFixturePath('file2.json');
+    expect(genDiff(before, after)).toEqual(expected);
+  });
+  test('flat yml files', () => {
+    const expected = '{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}';
+    const before = getFixturePath('file1.yml');
+    const after = getFixturePath('file2.yml');
+    expect(genDiff(before, after)).toEqual(expected);
+  });
 });
